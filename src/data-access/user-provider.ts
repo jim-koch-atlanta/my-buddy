722ae8049e8ac6fd031e3c80ab3bc9a09d18de38
Jim <jimkoch@gmail.com>
2026-05-31 20:23:31 -0400

    User model.

---
+++ src/data-access/user-provider.ts
@@ -0,0 +1,26 @@
+import { User } from '../models/user';
+import { getUserPoolProvider } from './user-pool-provider';
+
+export class UserProvider {
+    public static async getUserById(id: string): Promise<User | null> {
+        const { rows } = await getUserPoolProvider(id).query(
+            `SELECT id, email, name, timezone, cognito_sub, email_verified, status, created_at
+            FROM users WHERE id=$1`, [ id ]
+        );
+
+        if (rows.length != 1) {
+            return null;
+        }
+
+        return {
+            id: rows[0].id,
+            email: rows[0].email,
+            name: rows[0].name,
+            timezone: rows[0].timezone,
+            cognitoSub: rows[0].cognito_sub,
+            emailVerified: rows[0].email_verified,
+            status: rows[0].status,
+            createdAt: rows[0].created_at
+        }
+    }
+}
---
+++ src/models/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id: string; // uuid
+    email: string;
+    name: string;
+    timezone: string;
+    cognitoSub: string;
+    emailVerified: boolean;
+    status: string;
+    createdAt: string;
+}
